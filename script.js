fde15e4a34b0f398b3c83e61670439a45e86a2b3
Sujeet Yadav <99132296+sujeetyadav485@users.noreply.github.com>
2023-02-24 12:32:01 +0530

    Submit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//your code here
+// Get all the draggable divs
 const divs = document.querySelectorAll('.image');
 
 // Set the position of each div to absolute
@@ -8,23 +8,27 @@ divs.forEach((div) => {
 
 // Initialize the dragStartIndex variable
 let dragStartIndex;
+
+// Add a dragstart event listener to each div
 divs.forEach((div, index) => {
-    div.addEventListener('dragstart', () => {
-      dragStartIndex = index;
-    });
+  div.addEventListener('dragstart', () => {
+    dragStartIndex = index;
   });
-  
-  // Add a dragover event listener to each div
-  divs.forEach((div) => {
-    div.addEventListener('dragover', (e) => {
-      e.preventDefault();
-    });
+});
+
+// Add a dragover event listener to each div
+divs.forEach((div) => {
+  div.addEventListener('dragover', (e) => {
+    e.preventDefault();
   });
-  divs.forEach((div, index) => {
-    div.addEventListener('drop', () => {
-      // Swap the background images of the two divs
-      let temp = divs[dragStartIndex].style.backgroundImage;
-      divs[dragStartIndex].style.backgroundImage = div.style.backgroundImage;
-      div.style.backgroundImage = temp;
-    });
+});
+
+// Add a drop event listener to each div
+divs.forEach((div, index) => {
+  div.addEventListener('drop', () => {
+    // Swap the background images of the two divs
+    let temp = divs[dragStartIndex].style.backgroundImage;
+    divs[dragStartIndex].style.backgroundImage = div.style.backgroundImage;
+    div.style.backgroundImage = temp;
   });
+});
